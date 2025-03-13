23a7ae2f897efc97b7691ee2f295ba24c11531eb
Geovane Campos <geovanetcampos73@gmail.com>
2025-03-13 15:56:50 -0300

    Primeiro commit

---
+++ ola.js
@@ -0,0 +1,7 @@
+console.log('Olá mundo');
+console.log('teste de git'); 
+let a="100";
+let b="20";
+console.log('Mensagem de teste:',a);
+console.log('mensagem de teste:',b);
+console.log('Teste de alteração');
